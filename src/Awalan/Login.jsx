ecc5d79c3967f81355eb69bb816e1a3224a15a82
NarendraSaktiWw <yurayurawa@gmail.com>
2025-10-09 15:05:32 +0700

    Grup_Tagihan

---
+++ src/Awalan/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <h1 className='flex text-3xl font-bold text-blue-800'>HALO SAYA SAKTI</h1>
+    </div>
+  )
+}
+
+export default Login
